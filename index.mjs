36a39aeb46bc083f5280002b9de0cd69c2a87818
bradocchs <bradspencer@covenantchristian.org>
2019-10-09 19:10:59 -0400

    setup es modules

---
+++ index.mjs
@@ -0,0 +1,9 @@
+import { CanvasService, SparkpostService, VNNService } from 'services'
+
+const canvas = new CanvasService
+const spark = new SparkpostService
+const vnn = new VNNService
+
+canvas.test()
+spark.test()
+vnn.test()
