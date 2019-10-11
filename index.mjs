0b189cfcef6173ddf8aecce6dc981e52f02a9805
bradocchs <bradspencer@covenantchristian.org>
2019-10-10 22:25:11 -0400

    now returns data

--- index.mjs
+++ index.mjs
@@ -4,6 +4,10 @@ const canvas = new CanvasService
 const spark = new SparkpostService
 const vnn = new VNNService
 
-canvas.getGrades(2359)
+canvas.getGrades(2359).then(data => {
+  for (const course of data) {
+    console.log(course.name, course.enrollments[0].computed_current_score)
+  }
+})
 // spark.test()
 // vnn.test()
