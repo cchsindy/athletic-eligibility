67f512b3bd6c9cf576d26741f339e6522f80e545
bradocchs <bradspencer@covenantchristian.org>
2019-10-31 17:03:16 -0400

    http2 not supported by Canvas API

--- index.mjs
+++ index.mjs
@@ -4,11 +4,10 @@ const canvas = new CanvasService
 const spark = new SparkpostService
 const vnn = new VNNService
 
-// canvas.getGrades(2359).then(data => {
-//   for (const course of data) {
-//     console.log(course.name, course.enrollments[0].computed_current_score)
-//   }
-// })
+canvas.getGrades(2359).then(data => {
+  for (const course of data) {
+    console.log(course.name, course.enrollments[0].computed_current_score)
+  }
+})
 // spark.test()
 // vnn.test()
-canvas.getGrades2(2359)
