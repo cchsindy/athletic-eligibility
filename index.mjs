06e0f513811a07fa1befe50ddbaba0a3ff0ecbab
bradocchs <bradspencer@covenantchristian.org>
2019-10-31 20:05:44 -0400

    added getRoster test

--- index.mjs
+++ index.mjs
@@ -4,10 +4,14 @@ const canvas = new CanvasService
 const spark = new SparkpostService
 const vnn = new VNNService
 
-canvas.getGrades(2359).then(data => {
-  for (const course of data) {
-    console.log(course.name, course.enrollments[0].computed_current_score)
+// canvas.getGrades(2359).then(data => {
+//   for (const course of data) {
+//     console.log(course.name, course.enrollments[0].computed_current_score)
+//   }
+// })
+// spark.test()
+vnn.getRoster().then(players => {
+  for (const player of players) {
+    console.log(player)
   }
 })
-// spark.test()
-// vnn.test()
